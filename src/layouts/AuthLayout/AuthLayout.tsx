4985e39d4a0513fa256d63403c846762e08360ef
DatBuiTien18102002 <datbui18102002@gmail.com>
2024-08-25 09:36:41 +0700

    update notification auth page

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,34 +1,36 @@
 import OverlaySignInForm from "@/layouts/AuthLayout/OverlaySignInForm";
 import OverlaySignUpForm from "@/layouts/AuthLayout/OverlaySignUpform";
-import { Navigate, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { SignInForm, SignUpForm } from "@/forms";
+import { useEffect } from "react";
 
 const AuthLayout = () => {
-  const isAuthenticated = false;
   const { pathname } = useLocation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      alert(
+        "Tài khoản với quyền truy cập admin \nEmail: admin@gmail.com \nPassword: 123456"
+      );
+    }, 1000);
+  }, []);
 
   return (
     <>
-      {isAuthenticated ? (
-        <Navigate to="/" />
-      ) : (
-        <>
-          <div className="w-full h-[100vh] flex items-center justify-center bg-gradient-to-b from-sky-dark  to-sky">
-            {pathname === "/sign-in" ? <div /> : <div className="showSignUp" />}
-            <div className="authForm relative w-[750px] h-[500px] max-md:h-[650px] max-w-full mx-[10px] bg-white rounded-[13px] overflow-hidden shadow-searchDetail">
-              <SignUpForm />
-              <SignInForm />
+      <div className="w-full h-[100vh] flex items-center justify-center bg-gradient-to-b from-sky-dark  to-sky">
+        {pathname === "/sign-in" ? <div /> : <div className="showSignUp" />}
+        <div className="authForm relative w-[750px] h-[500px] max-md:h-[650px] max-w-full mx-[10px] bg-white rounded-[13px] overflow-hidden shadow-searchDetail">
+          <SignUpForm />
+          <SignInForm />
 
-              <div className="overlay-container absolute bg-sky top-0 left-[60%] w-[40%] h-full overflow-hidden transition-form z-[10] max-md:hidden">
-                <div className="overlay relative bg-sky text-white left-[-150%]  w-[250%] h-full transition-form">
-                  <OverlaySignUpForm />
-                  <OverlaySignInForm />
-                </div>
-              </div>
+          <div className="overlay-container absolute bg-sky top-0 left-[60%] w-[40%] h-full overflow-hidden transition-form z-[10] max-md:hidden">
+            <div className="overlay relative bg-sky text-white left-[-150%]  w-[250%] h-full transition-form">
+              <OverlaySignUpForm />
+              <OverlaySignInForm />
             </div>
           </div>
-        </>
-      )}
+        </div>
+      </div>
     </>
   );
 };
